f3ec7c3e43e08a8ff3daa460d1281f9d9afd7756
Eduardo Villamor <villamore838@gmail.com>
2020-11-12 16:01:58 -0500

    Agregue comentarios de explicación

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+// requerimos la librería
 const express = require('express')
+
+// creamos la aplicación
 const app = express()
 
+// definimos una ruta `GET` al path `/` (la raíz) que simplemente
+// imprime un título h1
 app.get('/', (req, res) => {
-    res.send('<h1>Hola Mundo</h1>')
+  res.send('<h1>Hola Mundo</h1>')
 })
 
-app.listen(3000, ()=> console.log('Listen on port 3000!'))
+// iniciamos el servidor en el puerto 3000
+app.listen(3000, () => console.log('Listening on port 3000!'))
